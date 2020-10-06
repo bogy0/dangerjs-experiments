dd630d4cbd9da8375b4f9d0cf6c3cc2d8f88b84c
Balint Lendvai <lendvai.balint@gmail.com>
2020-10-06 12:01:15 +0200

    feat: add label configs

--- dangerfile.js
+++ dangerfile.js
@@ -3,15 +3,17 @@ import { message, danger } from "danger"
 const php = danger.git.fileMatch("**/*.php");
 const js = danger.git.fileMatch("src/**/*.js");
 
-console.log(JSON.stringify(php));
-console.log(JSON.stringify(js));
+const backendLabel = { color: '#F00',  description: 'Backend Related Change',  name: 'Backend' };
+const frontendLabel = { color: '#00F',  description: 'Frontend Related Change',  name: 'Frontend' };
 
-if (php.created || php.modified) {
-  danger.github.utils.createOrAddLabel('Backend');
+{"modified":false,"created":true,"deleted":false,"edited":true}
+
+if (php.created || php.modified || php.deleted || php.edited) {
+  danger.github.utils.createOrAddLabel(backendLabel);
 }
 
-if (js.created || js.modified) {
-  danger.github.utils.createOrAddLabel('Frontend');
+if (js.created || js.modified || js.deleted || js.edited) {
+  danger.github.utils.createOrAddLabel(frontendLabel);
 }
 
 message('Hello from Danger');
